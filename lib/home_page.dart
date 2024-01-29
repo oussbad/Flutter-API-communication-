d222c34fe82a2c9cc0e47a8413fb61c3f56cf7a4
oussbad <oussamabadreddine210@gmail.com>
2024-01-29 16:58:11 +0100

    API

---
+++ lib/home_page.dart
@@ -0,0 +1,49 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'model/team.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+  List<Team> teams = [];
+
+//get teams
+  Future getTeams() async {
+    var response = await http.get(Uri.https('balldontlie.io', 'api/v1/teams'));
+    print(response.body);
+    var jsonData = jsonDecode(response.body);
+    for (var eachTeam in jsonData['data']) {
+      final team =
+          Team(abbreviation: eachTeam['abbreviation'], city: eachTeam['city']);
+      teams.add(team);
+    }
+    print(teams.length);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    getTeams();
+    return Scaffold(
+      body: FutureBuilder(
+        future: getTeams(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return ListView.builder(
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(teams[index].abbreviation),
+                );
+              },
+            );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
